e7ad86006d9e8e943e6bccf95ab530eee9be9ca9
WaltVenditi <waltvenditti@gmail.com>
2022-05-10 07:26:09 -0400

    connected to mongodb, user signup form successfully adds users to the database.

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// connect to mongodb
+var mongoose = require("mongoose");
+var mongoDB = process.env.MONGODB_URL;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
